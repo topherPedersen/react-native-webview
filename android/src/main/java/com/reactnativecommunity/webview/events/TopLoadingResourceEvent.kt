7750c508696ce51bb82cfc5425ab141b497aa48a
Christopher Pedersen <chris@topherpedersen.com>
2021-02-03 12:28:51 -0600

    remove commented-out code from TopLoadingResourceEvent.kt

--- android/src/main/java/com/reactnativecommunity/webview/events/TopLoadingResourceEvent.kt
+++ android/src/main/java/com/reactnativecommunity/webview/events/TopLoadingResourceEvent.kt
@@ -4,32 +4,12 @@ import com.facebook.react.bridge.WritableMap
 import com.facebook.react.uimanager.events.Event
 import com.facebook.react.uimanager.events.RCTEventEmitter
 
-/*
-class TopLoadingResourceEvent(viewId: Int, private val mEventData: WritableMap) :
-  Event<TopLoadingResourceEvent>(viewId) {
-  companion object {
-    const val EVENT_NAME = "topLoadingResource"
-  }
-
-  override fun getEventName(): String = EVENT_NAME
-
-  override fun canCoalesce(): Boolean = false
-
-  override fun getCoalescingKey(): Short = 0
-
-  override fun dispatch(rctEventEmitter: RCTEventEmitter) =
-    rctEventEmitter.receiveEvent(viewTag, eventName, mEventData)
-}
-*/
-
-
-// NOTE: Above code does not work, let's try using some code that does work and just change the name...
-
 /**
- * Event emitted when shouldOverrideUrlLoading is called
+ * Event emitted when onLoadResource is called
  */
-class TopLoadingResourceEvent(viewId: Int, private val mData: WritableMap) : Event<TopShouldStartLoadWithRequestEvent>(viewId) {
+class TopLoadingResourceEvent(viewId: Int, private val mData: WritableMap) : Event<TopLoadingResourceEvent>(viewId) {
   companion object {
+    // TODO: Change EVENT_NAME (will likely need to tweak React-Native/TypeScript code to do this
     const val EVENT_NAME = "topShouldStartLoadWithRequest"
   }
 
